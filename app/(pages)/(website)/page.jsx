92aa2e8544c4dcdb22552273b602f9133d5c7dc0
Hybler Group <adminit@hyblergroup.com>
2025-11-05 20:37:18 +0100

    Change PDF source in FlipbookViewer component

--- app/(pages)/(website)/page.jsx
+++ app/(pages)/(website)/page.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Page = () => {
   return (
     <div className="block">
-      <FlipbookViewer pdfUrl='/Pameti_VJH_B5_2025_kniha_nahled_P.pdf' />
+      <FlipbookViewer pdfUrl='/demo.pdf' />
     </div>
   )
 }
